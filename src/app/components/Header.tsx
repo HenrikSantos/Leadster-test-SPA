3245d44d052ef911e5a8d1e2eabdfaa526159cf0
Henrik Santos <henrik.ruan4@gmail.com>
2023-06-28 21:45:32 -0300

    feat: Header

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Image from 'next/image';
+
+export default function Header() {
+  return (
+    <header>
+      <Image src="/images/logo.png" alt="logo" width={100} height={100}/>
+    </header>
+  );
+}
