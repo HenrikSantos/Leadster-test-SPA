1608e6fc87c57efbc96c2814df4cdfa5b3efb13f
Henrik Santos <henrik.ruan4@gmail.com>
2023-06-29 08:26:09 -0300

    feat: Header style

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -3,8 +3,8 @@ import Image from 'next/image';
 
 export default function Header() {
   return (
-    <header>
-      <Image src="/images/logo.png" alt="logo" width={100} height={100}/>
+    <header className='flex justify-center py-5'>
+      <Image className='w-36' src="/images/logo.png" alt="logo" width={100} height={100}/>
     </header>
   );
 }
